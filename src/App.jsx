75f902acfe39b9525a1e32c62a2c7d87a10818a8
Anderson Valencia <avalenciacs@gmail.com>
2026-01-16 01:34:01 +0100

    update header-logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,62 @@
-
 import { Routes, Route } from "react-router-dom";
 import IsPrivate from "./components/IsPrivate";
+import Header from "./components/Header";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import FlatsList from "./pages/FlatsList";
 import FlatDetails from "./pages/FlatDetails";
 import FlatBalance from "./pages/FlatBalance";
-import FlatDashboard from "./pages/FlatDashboard"; 
+import FlatDashboard from "./pages/FlatDashboard";
 
 function App() {
   return (
-    <Routes>
-      {/* Public */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+    <>
+      <Header />
 
-      {/* Private */}
-      <Route
-        path="/"
-        element={
-          <IsPrivate>
-            <FlatsList />
-          </IsPrivate>
-        }
-      />
+      <Routes>
+        {/* Public */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
 
-      <Route
-        path="/flats/:flatId"
-        element={
-          <IsPrivate>
-            <FlatDetails />
-          </IsPrivate>
-        }
-      />
+        {/* Private */}
+        <Route
+          path="/"
+          element={
+            <IsPrivate>
+              <FlatsList />
+            </IsPrivate>
+          }
+        />
 
-      <Route
-        path="/flats/:flatId/balance"
-        element={
-          <IsPrivate>
-            <FlatBalance />
-          </IsPrivate>
-        }
-      />
+        <Route
+          path="/flats/:flatId"
+          element={
+            <IsPrivate>
+              <FlatDetails />
+            </IsPrivate>
+          }
+        />
 
-      <Route
-        path="/flats/:flatId/dashboard"
-        element={
-          <IsPrivate>
-            <FlatDashboard />
-          </IsPrivate>
-        }
-      />
-    </Routes>
+        <Route
+          path="/flats/:flatId/balance"
+          element={
+            <IsPrivate>
+              <FlatBalance />
+            </IsPrivate>
+          }
+        />
+
+        <Route
+          path="/flats/:flatId/dashboard"
+          element={
+            <IsPrivate>
+              <FlatDashboard />
+            </IsPrivate>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="w-full py-6 flex justify-center border-b">
-      <Link to="/">
-        <img
-  src="/logo-roomie.svg"
-  alt="Roomie logo"
-  className="h-16 w-auto md:h-20 lg:h-24"
-/>
-      </Link>
+    <header className="w-full border-b border-slate-200 bg-slate-100">
+      <div className="mx-auto flex h-20 max-w-6xl items-center justify-center px-4">
+        <Link to="/" aria-label="Go to home">
+          <img
+            src="/logo-roomie.svg"
+            alt="Roomie"
+            className="h-12 w-auto md:h-14"
+          />
+        </Link>
+      </div>
     </header>
   );
 }
